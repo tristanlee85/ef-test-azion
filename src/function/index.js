74361e8e3f7914d7e85b223f2fd69dd58801e610
Tristan Lee <tristanlee85@gmail.com>
2025-02-10 15:55:02 -0500

    Use EF w/ cheerio

--- src/function/index.js
+++ src/function/index.js
@@ -1,20 +1,41 @@
-import { isFriday } from "date-fns";
+import  * as cheerio  from "cheerio";
 
-/**
- * Handle Request
- * This is the input function where you can perform your implementation.
- * @param {Request} request
- * @param {*} args
- * @returns
- */
 async function handleRequest(request, args) {
-  /**
-   * In this template we are using the library https://www.npmjs.com/package/date-fns to
-   * check based on the date if today is Friday.
-   */
-  const message = isFriday(new Date()) ? "Yes, today is Friday! 😛" : "Oh no, today is not Friday! 😢";
+  const url = new URL(request.url);
+  const origin = new URL("https://www.google.com");
+  const originUrl = new URL(url.toString());
 
-  return new Response(message, { status: 200 });
+  originUrl.protocol = origin.protocol;
+  originUrl.host = origin.host;
+  originUrl.port = origin.port;
+
+  const response = await fetch(originUrl, {
+    headers: {
+      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+      host: "www.google.com",
+    },
+  });
+  const headers = new Headers(response.headers);
+  
+  console.log(`Incoming request: ${url.toString()} -> ${originUrl.toString()}`);
+  if (url.pathname === "/") {
+    const bodyText = await response.text();
+    const $ = cheerio.load(bodyText);
+    
+    $('title').text('My Custom Title');
+
+    const modifiedBody = $.html();
+    
+    headers.delete("content-length");
+    
+    return new Response(modifiedBody, {
+      status: response.status,
+      statusText: response.statusText,
+      headers,
+    });
+  }
+  
+  return response
 }
 
 export { handleRequest };
